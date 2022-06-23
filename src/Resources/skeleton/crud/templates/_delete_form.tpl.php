7565a4b96bcfbc393f646c4388088a498c9f1829
Gabriel <gabrielandrestoledo@gmail.com>
2022-06-23 15:06:15 -0300

    ajustando plantilla de delete

--- src/Resources/skeleton/crud/templates/_delete_form.tpl.php
+++ src/Resources/skeleton/crud/templates/_delete_form.tpl.php
@@ -1,6 +1,11 @@
 <form method="post" action="{{ path('<?= $route_name ?>_delete', {'<?= $entity_identifier ?>':
                                     <?= $entity_twig_var_singular ?>.<?= $entity_identifier ?>}) }}"
-      onsubmit="return confirm('Estás seguro de borrar este item?');" >
+      onsubmit="return confirm('{{'controller.messages.message_if_confirm_delete'| trans({}, 'controller_messages') }}');" >
     <input type="hidden" name="_token" value="{{ csrf_token('delete' ~ <?= $entity_twig_var_singular ?>.<?= $entity_identifier ?>) }}">
-    <button class="btn  btn-outline-danger">Borrar</button>
+    <button class="btn btn-danger"
+            data-toggle="tooltip"
+            data-placement="top"
+            title="{{'controller.titles.delete_entity'| trans({}, 'controller_messages') }}">
+            <i class="fa fa-times"></i>
+    </button>
 </form>
